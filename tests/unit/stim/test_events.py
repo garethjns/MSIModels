11e3068fcca984b2a9aab347a2617c57b46a28a2
garethjns <garethjns4@gmail.com>
2019-12-26 21:47:59 +0000

    Add multiple event combination

--- tests/unit/stim/test_events.py
+++ tests/unit/stim/test_events.py
@@ -145,10 +145,13 @@ class TestCompoundEvent(unittest.TestCase):
         noise_event = NoiseEvent(mag=0.3,
                                  fs=10000)
 
-        x, y = CompoundEvent._combiner(sine_event, noise_event)
+        x, y = CompoundEvent._combiner([sine_event, noise_event])
 
-        self.assertEqual(len(x), 10000)
-        self.assertEqual(len(y), 10000)
+        self.assertEqual(len(x), 200)
+        self.assertEqual(len(y), 200)
+
+        plt.plot(x, y)
+        plt.show()
 
     def test_combiner_with_non_matching_events(self):
         sine_event = SineEvent(fs=1000,
@@ -158,11 +161,66 @@ class TestCompoundEvent(unittest.TestCase):
                                  duration=1000,
                                  fs=1000)
 
-        x, y = CompoundEvent._combiner(sine_event, noise_event)
+        x, y = CompoundEvent._combiner([sine_event, noise_event])
 
         self.assertEqual(len(x), 1000)
-        self.assertEqual(len(y), 10000)
+        self.assertEqual(len(y), 1000)
 
+        plt.plot(x, y)
+        plt.show()
+
+    def test_combiner_with_multiple_non_matching_events(self):
+        sine_event_1 = SineEvent(fs=1000,
+                                 duration=1000,
+                                 start=0)
+        sine_event_2 = SineEvent(fs=1000,
+                                 duration=1000,
+                                 start=300)
+        noise_event = NoiseEvent(mag=0.3,
+                                 duration=1000,
+                                 fs=1000,
+                                 start=600)
+
+        x, y = CompoundEvent._combiner([sine_event_1, sine_event_2, noise_event])
+
+        self.assertEqual(len(x), 1600)
+        self.assertEqual(len(y), 1600)
+
+        plt.plot(x, y)
+        plt.show()
+
+    def test_combiner_with_equal_weighted_events(self):
+        constant_event_1 = Event(fs=1000,
+                                 duration=1000,
+                                 start=0)
+        constant_event_2 = Event(fs=1000,
+                                 duration=1000,
+                                 start=0)
+
+        x, y = CompoundEvent._combiner([constant_event_1,  constant_event_2])
+
+        self.assertAlmostEqual(max(y), 1, 3)
+
+        plt.plot(x, y)
+        plt.show()
+
+    def test_combiner_with_unequal_weighted_events(self):
+        constant_event_1 = Event(fs=1000,
+                                 duration=1000,
+                                 start=0)
+        constant_event_2 = Event(fs=1000,
+                                 duration=1000,
+                                 start=0)
+
+        x, y = CompoundEvent._combiner([constant_event_1,  constant_event_2],
+                                       weights=[0.5, 1])
+
+        self.assertAlmostEqual(max(y), 1.5, 3)
+
+        constant_event_1.plot()
+        constant_event_2.plot()
+        plt.plot(x, y)
+        plt.show()
 
     def test_sine_noise_multiply(self):
         sine_event = SineEvent(fs=10000)
@@ -171,7 +229,7 @@ class TestCompoundEvent(unittest.TestCase):
 
         compound_event = sine_event * noise_event
 
-        self.assertAlmostEqual(np.mean(sine_event.y), np.mean(compound_event.y), 1)
+        self.assertAlmostEqual(float(np.mean(sine_event.y)), float(np.mean(compound_event.y)), 1)
         self.assertGreater(np.std(compound_event.y), np.std(sine_event.y))
 
     def test_construct_from_list_of_2(self):
