6d69106d0e2fcbe72205dea79575cdb85bb423e8
garethjns <garethjns4@gmail.com>
2019-12-27 17:16:35 +0000

    Abstract Envelope, add tests

---
+++ stim/envelope.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from stim.base_signal import ms_to_pts
+
+
+class Envelope(ABC):
+    def __call__(self, y: np.ndarray) -> np.ndarray:
+        return self.f(y)
+
+    @abstractmethod
+    def f(self, y: np.ndarray) -> np.ndarray:
+        pass
+
+
+class ConstantEnvelope(Envelope):
+    def f(self, y: np.ndarray) -> np.ndarray:
+        return y
+
+
+class CosEnvelope(Envelope):
+    def f(self, y: np.ndarray) -> np.ndarray:
+        return y * (np.cos(np.linspace(1 * np.pi, 3 * np.pi, len(y))) + 1) * 0.5
+
+
+class CosRiseEnvelope(Envelope):
+    def __init__(self, fs: int, rise: int) -> None:
+        """
+
+        :param fs: Sampling rate in Hz.
+        :param rise: Rise in ms.
+        """
+        self.rise = rise
+        self.fs = fs
+
+    @property
+    def rise_pts(self) -> int:
+        return ms_to_pts(fs=self.fs,
+                         t_ms=self.rise)
+
+    def f(self, y: np.ndarray) -> np.ndarray:
+
+        cos_rise = (np.cos(np.linspace(1 * np.pi, 2 * np.pi, self.rise_pts)) + 1) * 0.5
+        cos_fall = np.flip(cos_rise)
+
+        uniform_centre = np.ones(shape=(len(y) - 2 * self.rise_pts))
+
+        envelope = np.concatenate((cos_rise, uniform_centre, cos_fall),
+                                  axis=0)
+
+        return y * envelope
---
+++ tests/unit/stim/test_envelope.py
@@ -0,0 +1,45 @@
+import unittest
+import numpy as np
+
+from stim.envelope import ConstantEnvelope, CosEnvelope, CosRiseEnvelope
+
+
+ONES = np.ones(shape=(1000, ))
+ZEROS = np.zeros(shape=(800, ))
+
+
+class TestConstantEnvelope(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.env = ConstantEnvelope()
+
+    def test_envelope_on_ones(self) -> None:
+        self.assertTrue(np.all(self.env(ONES) == ONES))
+
+    def test_envelope_on_zeros(self) -> None:
+        self.assertTrue(np.all(self.env(ZEROS) == ZEROS))
+
+
+class TestCosEnvelope(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.env = CosEnvelope()
+
+    def test_envelope_on_ones(self) -> None:
+        self.assertLess(np.sum(self.env(ONES)), np.sum(ONES))
+
+    def test_envelope_on_zeros(self) -> None:
+        self.assertTrue(np.all(np.round(self.env(ZEROS)) == ZEROS))
+
+
+class TestCosRiseEnvelope(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.env = CosRiseEnvelope(fs=1000,
+                                  rise=200)
+
+    def test_envelope_on_ones(self) -> None:
+        self.assertLess(np.sum(self.env(ONES)), np.sum(ONES))
+
+    def test_envelope_on_zeros(self) -> None:
+        self.assertTrue(np.all(self.env(ZEROS) == ZEROS))
